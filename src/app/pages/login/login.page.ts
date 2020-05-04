f40eda4bcbfc54ed14c768b54fe8e8e46d530f6a
jmorla <jorgemorlapro@gmail.com>
2020-05-03 20:12:24 -0400

    fixing login

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  username = '';
+  password = '';
+  error: boolean;
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  onLogin() {
+    if (this.username === 'paciente01' && this.password === 'paciente01') {
+      this.router.navigateByUrl('tabs/tab2');
+      this.username = '';
+      this.password = '';
+    } else {
+      this.error = true;
+    }
   }
 
 }
